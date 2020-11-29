6e81460c2e7e5d99393e5fc081a7156b23e1db5a
SerebryakovMA <serebryakov17m@mail.ru>
2020-11-29 15:35:01 +0300

    initial board

--- main.cpp
+++ main.cpp
@@ -3,30 +3,32 @@
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 using namespace std;
 
 int main() {
     srand(time(0));
-    int* counts[10];
+    double* counts[10];
 
     for (int i = 0; i < 10; i++) {
-        counts[i] = new int[i + 1];
+        counts[i] = new double[i + 1];
         for (int j = 0; j <= i; j++) {
             counts[i][j] = 0;
         }
     }
 const int Runs = 1000;
+const double coeff = 5 * 3 * 6.5821195144 * pow(10, -19);
 
 for (int run = 0; run < Runs; run++) {
     counts[0][0]++;
     int j = 0;
-    for (int; i = 1; i < 10; i++) {
+    for (int i = 1; i < 10; i++) {
         int r = rand() % 2;
         if (r == 1){
             j++;
         }
-        counts[i][j]++;
-    }
+        counts[i][j] += (1 - coeff * j * j);
+}
 }
 
 ofstream fout;
