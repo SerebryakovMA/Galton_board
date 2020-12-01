f84be9ae2dfbadd60e5f98536468b06646d0870e
SerebryakovMA <serebryakov17m@mail.ru>
2020-12-02 00:59:36 +0300

    unstable board with moving left

--- main.cpp
+++ main.cpp
@@ -4,63 +4,77 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
+#include <list>
+#include <iterator>
+#include <numeric>
 using namespace std;
+
+constexpr int num_rows = 15;
+constexpr int num_cols = 150;
+constexpr int central_col = (num_cols -1) / 2;
+
+void update_beans_in_cell_forward_backward(list<double> (&counts)[num_rows][num_cols], int row_idx, int col_idx){
+    auto& beans = counts[row_idx][col_idx];
+    auto& next_row_beans = counts[row_idx+1][col_idx];
+    auto& next_col_beans = counts[row_idx][min(col_idx+1, num_cols - 1)];
+    auto& prev_col_beans = counts[row_idx][max(col_idx-1, 0)];
+    const double coeff = 5 * 3 * 6.5821195144 * 1E-19;
+    for (auto bean = begin(beans); bean != end(beans); ++bean){
+        int r = rand() % 3;
+        if (r == 0){
+            next_row_beans.push_back(*bean * (1.0 - coeff * double((col_idx - central_col) * (col_idx - central_col))));
+        }
+        else if (r == 1) {
+            next_col_beans.push_back(*bean);
+        }
+        else if (r == 2) {
+            prev_col_beans.push_back(*bean);
+        }
+        bean = prev(beans.erase(bean));
+    }
+    for (auto bean = prev(end(beans)); bean != prev(begin(beans)); --bean){
+        int r = rand() % 3;
+        if (r == 0){
+            next_row_beans.push_back(*bean);
+        }
+        else if (r == 1) {
+            next_col_beans.push_back(*bean);
+        }
+        else if (r == 2) {
+            prev_col_beans.push_back(*bean);
+        }
+        bean = prev(beans.erase(bean));
+    }
+}
 
 int main() {
     srand(time(0));
-    constexpr int num_rows = 11;
-    struct gap {
-        int num;
-        double mass;
-    };
-    gap* counts[num_rows];
+    list<double> counts[num_rows][num_cols];
 
-    for (int i = 0; i < num_rows; i++) {
-        counts[i] = new gap[num_rows];
-        for (int j = 0; j <= i; j++) {
-            counts[i][j].mass = 0;
-            counts[i][j].num = 0;
-        }
-    }
-    const int Runs = 100;
-    const double coeff = 5 * 3 * 6.5821195144 * pow(10, -19);
+    const int beans = 1000;
+    const double mass = 1.0;
 
-    for (int run = 0; run < Runs; run++) {
-        counts[(num_rows -1) / 2][0].num++;
-        int j = 0;
-        for (int i = 1; i < num_rows-1; i++) {
-            int r = rand() % 3;
-            if (r == 0){
-                counts[i][j].num--;
-                counts[i][j-1].num++;
-                counts[i][j-1].mass += 1;
-            }
-            if (r == 1) {
-                counts[i][j].num--;
-                counts[i+1][j].num++;
-                counts[i+1][j].mass += 1;
-            }
-            if (r == 2) {
-                counts[i][j].num--;
-                counts[i][j+1].num++;
-                counts[i][j+1].mass += 1;
+    counts[0][central_col].resize(beans, mass);
+    const int Runs = 1000;
+    for (int run = 0; run < Runs; ++run){
+        for (int row = num_rows - 2; row >= 0; --row) {
+            for (int col = 0; col < num_cols; col++) {
+                update_beans_in_cell_forward_backward(counts, row, col);
             }
         }
     }
 
     ofstream fout;
     fout.open("output.csv");
-    for (int i = 0; i < num_rows; i++){
-        for (int j = 0; j <= i; j++){
-            fout << setw(4) << counts[i][j].num << ",";
+    // for (int row = 0; row < num_rows; row++){
+        int row = num_rows -1 ;
+        for (int j = 0; j < num_cols; j++){
+            fout << setprecision(5) << setw(7) << fixed << accumulate(counts[row][j].begin(), counts[row][j].end(), 0.0) << ",";
         }
         fout << endl;
-}
-fout.close();
+    // }
+    fout.close();
 
-for (int i = 0; i < num_rows; i++){
-    delete[] counts[i];
-}
 
 return 0;
 }
