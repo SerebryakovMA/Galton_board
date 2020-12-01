377415f15e0cf6ad18a05ea90d27e5e564352129
SerebryakovMA <serebryakov17m@mail.ru>
2020-12-01 21:51:55 +0300

    fixes

--- main.cpp
+++ main.cpp
@@ -22,37 +22,39 @@ int main() {
             counts[i][j].num = 0;
         }
     }
-const int Runs = 100;
-const double coeff = 5 * 3 * 6.5821195144 * pow(10, -19);
+    const int Runs = 100;
+    const double coeff = 5 * 3 * 6.5821195144 * pow(10, -19);
 
-for (int run = 0; run < Runs; run++) {
-    counts[(num_rows -1) / 2][0].num++;
-    int j = 0;
-    for (int i = 1; i < num_rows; i++) {
-        int r = rand() % 3;
-        if (r == 0){
-            counts[i][j-1].num++;
-            counts[i][j-1].mass += 1;
+    for (int run = 0; run < Runs; run++) {
+        counts[(num_rows -1) / 2][0].num++;
+        int j = 0;
+        for (int i = 1; i < num_rows-1; i++) {
+            int r = rand() % 3;
+            if (r == 0){
+                counts[i][j].num--;
+                counts[i][j-1].num++;
+                counts[i][j-1].mass += 1;
+            }
+            if (r == 1) {
+                counts[i][j].num--;
+                counts[i+1][j].num++;
+                counts[i+1][j].mass += 1;
+            }
+            if (r == 2) {
+                counts[i][j].num--;
+                counts[i][j+1].num++;
+                counts[i][j+1].mass += 1;
+            }
         }
-        if (r == 1) {
-            counts[i+1][j].num++;
-            counts[i+1][j].mass += 1;
-        }
-        if (r == 2) {
-            counts[i][j+1].num++;
-            counts[i][j+1].mass += 1;
-        }
-}
-}
-
-ofstream fout;
-fout.open("output.csv");
-for (int i = 0; i < num_rows; i++){
-    for (int j = 0; j <= i; j++){
-        fout << setw(4) << counts[i][j].num << ",";
     }
-    fout << endl;
 
+    ofstream fout;
+    fout.open("output.csv");
+    for (int i = 0; i < num_rows; i++){
+        for (int j = 0; j <= i; j++){
+            fout << setw(4) << counts[i][j].num << ",";
+        }
+        fout << endl;
 }
 fout.close();
 
